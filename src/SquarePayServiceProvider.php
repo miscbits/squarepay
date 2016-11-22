72382ea8dff022b2b12671abddca55083cc7eafa
miscbits <warthur@healthjump.com>
2016-11-22 16:47:44 -0500

    first commit. package created from boilerplate

---
+++ src/SquarePayServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Miscbits\SquarePay;
+
+use Illuminate\Support\ServiceProvider;
+
+class SquarePayServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
